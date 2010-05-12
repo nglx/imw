630a8200819b075f6d64a25abafe63bd3e51e182
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-05-12 02:05:31 -0500

    alias => alias_method.

--- lib/imw/repository.rb
+++ lib/imw/repository.rb
@@ -3,7 +3,7 @@ module IMW
   # A Repository is a collection of datasets.  It is used by the
   # command-line +imw+ tool.
   class Repository < Hash
-    alias :datasets, :values
+    alias_method :datasets, :values
   end
   
 end
