9b8cc1a46e39351989fd029f6f010ccb972e7c64
Travis Dempsey <travis@infochimps.com>
2011-06-07 09:56:24 -0500

    initial commit; fresh start for branch:new

--- old/lib/imw/chunk_store/uri_file_store.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-module IMW
-  module UriFileStore
-  end
-end
--- old/meta/imw_munger_template.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby 
-require 'imw'
-
-puts "I need instructions!  See http://help.infochimps.org/help/show/InfiniteMonkeywrench"
-
